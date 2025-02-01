38063e1a4e0fbd280c5b8107d3211728c4c5af16
Nikhil Kumar <nikhil@Nikhils-Mac-mini.fritz.box>
2025-02-01 14:31:10 +0100

    new messages added

--- script.js
+++ script.js
@@ -6,7 +6,14 @@ const messages = [
     "But… do you really want to break my heart? 🥺",
     "Come on, you know you want to! 😘",
     "I brought snacks and good vibes, just say yes.",
-    "I’ll give you unlimited cuddles if you say yes! 🤗"
+    "I’ll give you unlimited cuddles if you say yes! 🤗",
+    "Wait, wait! What if I throw in chocolate? 🍫",
+    "But... what if I promise to never eat the last slice of pizza? 🍕❤️",
+    "But think of all the cute selfies we can take together. 📸✨",
+    "Say yes, and I’ll make sure you always get the bigger slice of dessert. 🍨",
+    "I’ll take you on late-night ice cream runs... even in pajamas. 🍦",
+    "What if I promise to never steal the blankets again (ok, maybe just a little)? 🛏️",
+    "I’m literally one YES away from doing a happy dance. 🕺💥"
 ];
 
 let messageIndex = 0;
